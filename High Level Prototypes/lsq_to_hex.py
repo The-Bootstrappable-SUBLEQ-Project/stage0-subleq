a57b8301123c105c3a63756d7ffc3d55cb41d8d0
Nyan Cat <17372086+e666666@users.noreply.github.com>
2023-01-02 18:11:33 +0800

    *.py: Fix styling

--- High Level Prototypes/lsq_to_hex.py
+++ High Level Prototypes/lsq_to_hex.py
@@ -16,7 +16,10 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with stage0-subleq.  If not, see <http://www.gnu.org/licenses/>.
 """
+# https://stackoverflow.com/a/62775680
+from __future__ import annotations
 import sys
+from dataclasses import dataclass
 
 # Well...now I have to worry about these
 lsq_insts = ["var", "label", "addr",
@@ -24,53 +27,74 @@ lsq_insts = ["var", "label", "addr",
              "subaddr", "zeroaddr",
              "raw", "rem"]
 
-# Entry format: [Inst, Tokens, Comment]
-lines = []
+
+@dataclass
+class Line:
+    inst: str
+    tokens: list[str]
+    comment: str = ""
+
+
+@dataclass
+class Reference:
+    lineNum: int
+    tokenId: int
+
+
+@dataclass
+class Symbol:
+    addr: int
+    refCount: int = 0
+    refs: list[Reference] = []
+
 
 # TODO: Take hex_version as a parameter
 hex_version = 0
 # Only hex0 is supported currently
 assert hex_version == 0
 
+lines = []
 inp = open(sys.argv[1]).read().split("\n")
 for line in inp:
     if len(line.strip()) == 0:
-        lines.append(["newline", [], ""])
+        lines.append(Line("newline"))
         continue
 
     tokens = line.split()
-    if tokens[0] == "rem":
-        lines.append(["rem", [], " ".join(tokens[1:])])
+    inst = tokens[0]
+    if inst == "rem":
+        lines.append(Line("rem", [], " ".join(tokens[1:])))
         continue
-    elif tokens[0] in lsq_insts:
-        lines.append([tokens[0], tokens[1:], ""])
+    elif inst in lsq_insts:
+        lines.append(line(inst, tokens[1:]))
     else:
-        raise SyntaxError(f"Unknown instruction: {tokens[0]}")
+        raise SyntaxError(f"Unknown instruction: {inst}")
 
 # Entry format: [Address, RefCount, References (LineNum, TokenId)]
 symbols = {}
 for line in lines:
     if line[0] in ["var", "label", "addr"]:
-        symbols[line[1][0]] = [int(line[1][1], 16) if line[0] == "addr" else None, 0, []]
+        symbols[line.tokens[0]] = Symbol(int(line[1][1], 16) if line[0] == "addr" else None)
 
 # Count references
 for line in lines:
     if line[0] in ["abssq", "relsq", "lblsq"]:
-        symbols[line[1][0]][1] += 1
-        symbols[line[1][1]][1] += 1
+        symbols[line.tokens[0]].refCount += 1
+        symbols[line.tokens[1]].refCount += 1
         if line[0] == "lblsq":
-            symbols[line[1][2]][1] += 1
+            symbols[line.tokens[2]].refCount += 1
     elif line[0] == "subaddr":
-        symbols[line[1][1]][1] += 1
+        symbols[line.tokens[1]].refCount += 1
 print(symbols)
 
 i = 0
 while i < len(lines):
-    if lines[i][0] not in ["subaddr", "zeroaddr"]:
+    if lines[i].inst not in ["subaddr", "zeroaddr"]:
         i += 1
         continue
-    sym = lines[i][1][0]
-    if lines[i][0] == "subaddr":
-        lines[i:i+1] = [f"relsq {sym}_decaddr_{x} {lines[i][1][1]} 1"]
+    sym = lines[i].tokens[0]
+    if lines[i].inst == "subaddr":
+        lines[i:i + 1] = [Line("relsq", [f"{sym}_subaddr_{x}", f"{lines[i][1][1]}", "1"]) for x in range(symbols[sym].refCount)]
     i += 1
     pass
+print(lines)
