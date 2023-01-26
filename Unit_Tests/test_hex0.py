bbb20264e9fb62f2ff59518be1ae68bc7f4d2b93
Nyan Cat <17372086+e666666@users.noreply.github.com>
2023-01-26 16:43:19 +0800

    Add more hex0/hex0_monitor tests

--- Unit_Tests/test_hex0.py
+++ Unit_Tests/test_hex0.py
@@ -4,3 +4,11 @@ from .utils import runEmu
 
 def test_hex0_hello_world():
     assert runEmu("../phase0-hex/hex0", ["../Examples/hello_world.hex0"], 30) == open("../Examples/hello_world.bin", "rb").read()
+
+
+def test_hex0_hex0_monitor():
+    assert runEmu("../phase0-hex/hex0", ["../phase0-hex/hex0_monitor.hex0"], 30) == open("../phase0-hex/hex0_monitor.bin", "rb").read()
+
+
+def test_hex0_hex0():
+    assert runEmu("../phase0-hex/hex0", ["../phase0-hex/hex0.hex0"], 30) == open("../phase0-hex/hex0.bin", "rb").read()
--- Unit_Tests/test_hex0_monitor.py
+++ Unit_Tests/test_hex0_monitor.py
@@ -22,3 +22,9 @@ def test_hex0_monitor_load_ten_pass():
     assert runEmu("../phase0-hex/hex0_monitor",
                   ["../phase0-hex/hex0_monitor.hex0"] * 10 + ["../Examples/hello_world.hex0"],
                   30) == b"Hello, World!\n"
+
+
+def test_hex0_monitor_hex0():
+    assert runEmu("../phase0-hex/hex0_monitor",
+                  ["../phase0-hex/hex0.hex0", "../Examples/hello_world.hex0"],
+                  30) == open("../Examples/hello_world.bin", "rb").read()
