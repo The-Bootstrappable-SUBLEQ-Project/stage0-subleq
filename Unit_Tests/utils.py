9ffbebfe4243d2f63dde1ae8f4ce2d96752024cc
Nyan Cat <17372086+e666666@users.noreply.github.com>
2023-01-27 16:35:45 +0800

    Use random path name for temp test files

--- Unit_Tests/utils.py
+++ Unit_Tests/utils.py
@@ -1,20 +1,35 @@
 #!/usr/bin/python3
 import subprocess
+import string
+import random
+import os
 
-tempFilePath = "/tmp/stage0_subleq_test_batch_input"
+
+def getTempFilePath():
+    return "/tmp/stage0_subleq_test_batch_input_" + ''.join(random.choice(string.ascii_letters) for i in range(20))
 
 
 def runEmu(base_path, inps=None, timeout=3):
+    tempFilePath = None
     if inps is None or len(inps) == 0:
         # Feed some random file
         batch_input = "../LICENSE"
     elif len(inps) == 1:
         batch_input = inps[0]
     else:
+        tempFilePath = getTempFilePath()
         with open(tempFilePath, "wb") as tempFile:
             for path in inps:
                 with open(path, "rb") as f:
                     tempFile.write(f.read())
         batch_input = tempFilePath
 
-    return subprocess.check_output(["../../noontide-emu/target/release/noontide-emu", "-b", batch_input, base_path], timeout=timeout)
+    try:
+        ret = subprocess.check_output(["../../noontide-emu/target/release/noontide-emu", "-b", batch_input, base_path], timeout=timeout)
+    except Exception:
+        ret = f"Error: Timeout after {timeout}s"
+    finally:
+        if tempFilePath is not None:
+            os.remove(tempFilePath)
+
+    return ret
