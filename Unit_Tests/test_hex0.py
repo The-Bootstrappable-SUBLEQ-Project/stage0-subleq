6863ad22d9d39d4813c99329f388107817bcf6ff
Nyan Cat <17372086+e666666@users.noreply.github.com>
2023-01-27 17:03:21 +0800

    Add test_hex0_everything_else

--- Unit_Tests/test_hex0.py
+++ Unit_Tests/test_hex0.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 from .utils import runEmu
+import pytest
+import glob
 
 
 def test_hex0_hello_world():
@@ -12,3 +14,15 @@ def test_hex0_hex0_monitor():
 
 def test_hex0_hex0():
     assert runEmu("../phase0-hex/hex0", ["../phase0-hex/hex0.hex0"], 30) == open("../phase0-hex/hex0.bin", "rb").read()
+
+
+@pytest.mark.slower
+def test_hex0_everything_else():
+    alreadyTested = ["../Examples/hello_world.hex0", "../phase0-hex/hex0_monitor.hex0", "../phase0-hex/hex0.hex0"]
+    hex0Files = glob.glob("../**/*.hex0")
+    for path in hex0Files:
+        if path in alreadyTested:
+            continue
+        basePath = path[:-5]
+        print("Confirming", basePath)
+        assert runEmu("../phase0-hex/hex0", [basePath + ".hex0"], 300) == open(basePath + ".bin", "rb").read()
