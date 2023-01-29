838808b8c452b603a8452efd2077dfdec08ebdaf
Nyan Cat <17372086+e666666@users.noreply.github.com>
2023-01-29 11:42:06 +0800

    Add test_hex1_monitor_bootstrap

--- Unit_Tests/test_hex1_monitor.py
+++ Unit_Tests/test_hex1_monitor.py
@@ -22,3 +22,10 @@ def test_hex1_monitor_load_ten_pass():
     assert runEmu("../phase0-hex/hex1_monitor",
                   ["../phase0-hex/hex1_monitor.hex1"] * 10 + ["../Examples/hello_world.hex1"],
                   30) == b"Hello, World!\n"
+
+
+def test_hex1_monitor_bootstrap():
+    assert runEmu("../phase0-hex/hex0_monitor",
+                  ["../phase0-hex/hex0_monitor.hex0", "../phase0-hex/hex0_monitor.hex0",
+                   "../phase0-hex/hex1_monitor.hex0", "../phase0-hex/hex1_monitor.hex1", "../phase0-hex/hex1_monitor.hex1",
+                   "../Examples/hello_world.hex1"], 10) == b"Hello, World!\n"
